b708a5d1446136a02d1098ec62362f094b33cef2
danOdo <danielodobasic@hotmail.de>
2025-11-06 13:34:43 +0100

    galerie fertig

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 interface GalleryItem {
@@ -12,72 +12,16 @@ interface GalleryItem {
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent implements OnInit {
-  protected readonly galleryItems: GalleryItem[] = [
-    {
-      image: 'assets/city5.webp'
-    },
-    {
-      image: 'assets/city6.webp'
-    },
-    {
-      image: 'assets/city8.webp'
-    },
-    {
-      image: 'assets/city9.webp'
-    }
+export class GalleryComponent {
+  private readonly baseItems: GalleryItem[] = [
+    { image: 'assets/city2.webp' },
+    { image: 'assets/city4.webp' },
+    { image: 'assets/city5.webp' },
+    { image: 'assets/city6.webp' },
+    { image: 'assets/city8.webp' },
+    { image: 'assets/city9.webp' }
   ];
 
-  protected readonly currentIndex = signal(0);
-  private readonly destroyRef = inject(DestroyRef);
-  private slideTimer: ReturnType<typeof setInterval> | null = null;
-  private readonly intervalMs = 2500;
-
-  constructor() {
-    this.destroyRef.onDestroy(() => this.clearTimer());
-  }
-
-  ngOnInit(): void {
-    this.startAutoSlide();
-  }
-
-  protected nextSlide(): void {
-    this.currentIndex.update(idx => (idx + 1) % this.galleryItems.length);
-  }
-
-  protected previousSlide(): void {
-    this.currentIndex.update(idx => (idx - 1 + this.galleryItems.length) % this.galleryItems.length);
-  }
-
-  protected goToSlide(index: number): void {
-    if (index === this.currentIndex()) {
-      return;
-    }
-    this.currentIndex.set(index);
-    this.restartAutoSlide();
-  }
-
-  protected pauseAutoSlide(): void {
-    this.clearTimer();
-  }
-
-  protected resumeAutoSlide(): void {
-    this.startAutoSlide();
-  }
-
-  private startAutoSlide(): void {
-    this.clearTimer();
-    this.slideTimer = setInterval(() => this.nextSlide(), this.intervalMs);
-  }
-
-  private restartAutoSlide(): void {
-    this.startAutoSlide();
-  }
-
-  private clearTimer(): void {
-    if (this.slideTimer) {
-      clearInterval(this.slideTimer);
-      this.slideTimer = null;
-    }
-  }
+  protected readonly marqueeItems = [...this.baseItems, ...this.baseItems];
+  protected readonly itemCount = this.baseItems.length;
 }
